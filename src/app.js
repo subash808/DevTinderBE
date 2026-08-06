636bc73b94daaabf7527c344424831cd1b14a4d9
subash808 <s.k.subash062003@gmail.com>
2026-08-06 21:36:23 +0530

    playing with rooutes and multiple route handler

--- src/app.js
+++ src/app.js
@@ -4,8 +4,27 @@ const port = 3000
 
 const app = express()
 
-app.use((req, res) => {
-    res.send('Hello, this is subash developer!!!')
+app.use('/users', (req, res) => {
+    res.send('Hello, this is subash developer users!!!')
+})
+
+app.use('/test', (req, res, next) => {
+    console.log('first route handler')
+
+    // this next function passes the control to the next route handler
+    // that handler send to the callstack queue and then this function execute after that handler is executed
+    // when after that handler executed, this handler send to the callstack queue and this function 
+    // execute and return error because response is already sent to the client and this function try to send response again
+
+    // so always use next() after response send to the client
+    // next()  
+    res.send('this is first route handler')
+    next()
+
+}, (req, res) => {
+    console.log('second route handler')
+
+    res.send('this is second route handler')
 })
 
 app.listen(port, () => {
