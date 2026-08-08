d1e5a9b920f3e453576ff44594339e2db59c1ecb
Subash.SK <157565202+subash808@users.noreply.github.com>
2026-08-08 22:53:30 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     const connect = await mongoose.connect(
-        "mongodb+srv://sksubash062003_db_user:oXAYp1zvJQU6NfkA@subashdb.44t1ia9.mongodb.net/DevTinder"
+        "testing"
     )
 }
 
